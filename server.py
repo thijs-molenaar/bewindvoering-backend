e5da57bf35e626a2c86afef24c9526ed597f410d
Thijs Molenaar <thijsmolenaar+gh@gmail.com>
2017-10-22 13:19:28 +0200

    Adds server.ini to gitignore. Use server.ini as configfile in settings.py instead of server.py.

--- server.py
+++ server.py
@@ -1,10 +1,4 @@
 from eve import Eve
-import configparser
-
-Config = configparser.ConfigParser()
-Config.read("server.ini")
-
-print(Config.get('mongo', 'username'))
 
 app = Eve()
 app.run()
--- settings.py
+++ settings.py
@@ -1,1 +1,13 @@
+import configparser
+
+Config = configparser.ConfigParser()
+Config.read("server.ini")
+
 DOMAIN = {'people': {}}
+
+# MongoDB
+MONGO_HOST = Config.get('mongo', 'host')
+MONGO_PORT = Config.get('mongo', 'port')
+MONGO_USERNAME = Config.get('mongo', 'username')
+MONGO_PASSWORD = Config.get('mongo', 'password')
+MONGO_DBNAME = Config.get('mongo', 'dbname')
